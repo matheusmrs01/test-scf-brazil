1f252cb014079dd6b07056157fdfd29eb8cfc38a
Matheus Morais <matheus.silva.morais01@gmail.com>
2023-06-12 10:25:19 -0300

    fix: corrigindo teste 4

--- teste4.js
+++ teste4.js
@@ -1,13 +1,21 @@
 var data =  require("./fakeData");
 
 module.exports =  function(req, res) {
-  
-    var id =  req.query.id;
+    const { id } = req.query;
+    let { name, job } =  req.body;
+    let user = data.find(user => user.id === parseInt(id));
 
-    const reg = data.find(d => id == id);
-    reg.name = req.body.name;
-    reg.job = req.body.job;
+    if(!user){
+        return res.status(402).send({ message: `User with id ${id} not found.` });
+    }
 
-    res.send(reg);
+    let updatedUser = {
+        ...user, 
+        name, 
+        job
+    };
 
+    data[data.indexOf(user)] = updatedUser
+
+    return res.status(200).send(updatedUser);
 };
