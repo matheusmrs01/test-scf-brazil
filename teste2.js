569062eef4fb1bd52e9d1706f4bd009d17afde2c
Matheus Morais <matheus.silva.morais01@gmail.com>
2023-06-12 10:17:11 -0300

    fix: corrigindo atribuição de id no post de usuário

--- teste2.js
+++ teste2.js
@@ -3,7 +3,7 @@ const data =  require("./fakeData");
 module.exports = function(req, res){
     let { name, job } =  req.body;
     let newUser = {
-        id: data.length + 1,
+        id: data[data.length - 1].id + 1,
         name,
         job
     }
